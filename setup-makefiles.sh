8a1dc6107a248b1e00c5fea620276adb74410cc9
iamlordutkarsh <lalbaake456@gmail.com>
2020-10-31 16:13:55 +0000

    Cleanup && Changes

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -17,10 +17,31 @@
 
 set -e
 
-export DEVICE=cactus
-export DEVICE_COMMON=mt6765-common
-export VENDOR=xiaomi
+DEVICE=RMX2020
+VENDOR=realme
 
-export DEVICE_BRINGUP_YEAR=2019
+INITIAL_COPYRIGHT_YEAR=2020
 
-./"../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" $@
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+AOSP_ROOT="$MY_DIR"/../../..
+
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# Finish
+write_footers
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo lineage_o5prolte-userdebug
-add_lunch_combo lineage_o5prolte-eng
